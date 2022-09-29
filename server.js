f4e80d94a26973389f17ce21d461e74525c1d9e3
Codebob <111080125+alprojecttest@users.noreply.github.com>
2022-09-29 14:40:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,12 +25,11 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
 
-/*
 
 myEmitter.on('log', (msg) => logEvents(msg));
 
 setTimeout(() => {
     myEmitter.emit('log', 'Log emitted!')
-}, 3000)*/
+}, 3000)
 
 
