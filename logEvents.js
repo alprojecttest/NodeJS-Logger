2aae148fda33e667713211cef8f5fd77a72cf148
Codebob <111080125+alprojecttest@users.noreply.github.com>
2022-10-02 22:44:22 +0530

    Add files via upload

--- logEvents.js
+++ logEvents.js
@@ -4,20 +4,25 @@ const path = require('path');
 const { format } = require('date-fns');
 const { v4: uuid} = require('uuid');
 
-const logEvents = async (message) => {
+const logEvents = async (logData, logFile) => {
     const date = `${format(new Date(), 'HH:mm:ss\tdd/MM/yyyy')}`
-    const logitems = `${date}\t${uuid()}\t${(message)}\r`;
-    console.log(logitems)
+    const logitems = logData;
 
     try{
-        if(!fs.existsSync(path.join(__dirname, 'logs'))){
-           await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        if(!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+           await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'logger.txt'), logitems);
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFile), logitems);
     }
     catch(err){
         console.log(err)
     }
 }
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+    logEvents(`${req.url}\t${req.headers.origin}\r`, 'logger.txt')
+    next();
+};
+
+
+module.exports = logger;
